5fd09b2c89a1c7d9d9d8eed47570c064ad6e9e28
Ankith <srivatsankih.toopurani@gmail.com>
2025-01-30 19:12:16 +0530

    Merge strings alternatively

---
+++ MergeAlternateStrings.py
@@ -0,0 +1,18 @@
+def MergeAlternateString(str1,str2):
+    str3 = ""
+    mi = min(len(str1),len(str2))
+    
+    for i in range(mi):
+        str3 += str1[i] + str2[i]
+    
+    if len(str1) > mi:
+        str3 += str1[mi:]
+    elif len(str2) > mi:
+        str3 += str2[mi:]
+    
+    return str3
+
+str1 = input("Enter the first word")
+str2 = input("Enter the second word")
+
+print(MergeAlternateString(str1,str2))
